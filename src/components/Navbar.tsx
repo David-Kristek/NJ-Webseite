df3c197348fe1b7991c57b62299cb503f587897d
David Křístek <david.kristek05@gmail.com>
2022-01-17 19:57:28 +0100

    dark mode fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,18 +9,18 @@ export default function Navbar() {
   console.log(location);
 
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 h-auto">
+    <nav className="border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-800 h-auto">
       <div className="container flex flex-wrap justify-between items-center mx-auto ">
         <Link to="#" className="flex">
           <img src="./logo.png" className="h-10 mr-3" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             Guten Tag
           </span>
         </Link>
         <button
           data-collapse-toggle="mobile-menu"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2  hover:bg-gray-700 focus:ring-gray-600"
           aria-controls="mobile-menu-2"
           aria-expanded="false"
         >
